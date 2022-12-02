bf93fa5f08b7eed0ea8194cf4fb515273adfed87
vadim <vadimeray2006@gmail.com>
2022-12-02 20:25:56 +0800

    end

--- someWork/Program.cs
+++ someWork/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ThreeChar (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3) count++;
+    }
+    string[] resultArray = new string[count];
+    count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        
+        if(array[j].Length <= 3) 
+        {
+            resultArray[count] = array[j];
+            count++;
+        }
+    }
+    return resultArray;
+}
+void ShowStrArray (string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+string[] simbols = {"hi", "!) ", "Hello","its",  "Qwerty","What's your name?", " ME"};
+string[] myArray = ThreeChar(simbols);
+ShowStrArray(myArray);
